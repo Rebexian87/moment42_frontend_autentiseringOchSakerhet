6030171964bc2cde79fd8dcc51f9ad0fe51f1849
Rebecca Smith <rebecca.smith@hotmail.se>
2025-08-25 23:21:31 +0200

    added footer, updated main.js and css

--- js/main.js
+++ js/main.js
@@ -4,53 +4,56 @@ let menuEl=document.getElementById("headmenu");
 
 let logInFormEl =document.getElementById("logInForm")
 
+let flagForm=document.getElementById("flagForm")
+
+let registerForm=document.getElementById("registerForm")
+
+
+
+//Saker som sker när sidan startas
 window.onload=init;
 
 function init() {
     changeMenu()
-     getFlags()
+    let flaggor=document.getElementById("flaggor")
+    if(flaggor) {
+    getFlags()}
+     
 
     if(logInFormEl) {
-        logInFormEl.addEventListener("submit", loginUser)
-    }
+        logInFormEl.addEventListener("submit", loginUser)}
       
     if(flagForm) {
-      document.getElementById("addFlag").addEventListener("click", createFlag); }
-    if(registerForm) {
-       document.getElementById("submits").addEventListener("click", createUser); 
-    }
+      document.getElementById("addFlag").addEventListener("click", createFlag); 
+       }
+   
+       
 
    
     }
+document.getElementById("submits").addEventListener("click", createUser);  
 
-
+    //Function som visar meny beroende på om man har ett tokan eller inte
 function changeMenu(){
 
-    // localStorage.setItem("user_token", "testtest")
-
+   
     if(localStorage.getItem("user_token")) {
         menuEl.innerHTML= `
-                   <li class="liheadmenu"><a href="index.html">Startsida</a></li>
-          
-            
+            <li class="liheadmenu"><a href="index.html">Startsida</a></li>       
             <li class="liheadmenu"><a href="secretPage.html">secret</a></li>
-            <li class="liheadmenu"><button id="logoutButton">Logga ut</button></li>
-        
-        `
+            <li class="liheadmenu"><button id="logoutButton">Logga ut</button></li>`
 
     } else { 
         
            menuEl.innerHTML= `
-              <li class="liheadmenu"><a href="index.html">Startsida</a></li>
-             <li class="liheadmenu"><a href="register.html">Registrera dig</a></li>
-           
-               <li class="liheadmenu"><a href="loggaIn.html">Logga in</a></li>
-        
-        `
+                <li class="liheadmenu"><a href="index.html">Startsida</a></li>
+                <li class="liheadmenu"><a href="register.html">Registrera dig</a></li>
+                <li class="liheadmenu"><a href="loggaIn.html">Logga in</a></li>`
 
-           //<li class="liheadmenu"><a href="register.html">Startsida</a></li>
+           
     }
 
+    //Logoutbutton som tar bort token om man loggar ut. 
     let logoutButton= document.getElementById("logoutButton");
     if(logoutButton) {
         logoutButton.addEventListener("click", ()=> {
@@ -61,6 +64,7 @@ function changeMenu(){
     }
 }
 
+//Funktion för att kunna logga in en användare
 async function loginUser(e) {
     e.preventDefault();
         let userEl=document.getElementById("username").value;
@@ -80,7 +84,7 @@ async function loginUser(e) {
        try {
 
         const resp = await fetch("http://127.0.0.1:3001/api/login", {
-            method: "POST",
+            method: "POST", 
             headers: {
                 "content-type": "application/json"
             },
@@ -90,7 +94,7 @@ async function loginUser(e) {
         if(resp.ok) {
             const data = await resp.json();
             // console.log(data);
-            localStorage.setItem("user_token", data.response.token);
+            localStorage.setItem("user_token", data.response.token);//Sparar token i localstoraage
             window.location.href= "secretPage.html"
             console.log(data.response.token);
             
@@ -107,15 +111,16 @@ async function loginUser(e) {
 
 
 
-
-      async function createUser (username,  email, password){
+        //Funktion som skapar en användare
+      async function createUser (e){
+           e.preventDefault();
             let usernameEl=document.getElementById("username")
             let emailEl=document.getElementById("email")
             let passwordEl=document.getElementById("password")
 
-            username=usernameEl.value
-            email=emailEl.value
-            password=passwordEl.value
+            let username=usernameEl.value
+            let email=emailEl.value
+            let password=passwordEl.value
 
             let user = {  
             username: username,
@@ -137,7 +142,7 @@ async function loginUser(e) {
 
 
           
-
+        //Funtion som skapar en flagga
       async function createFlag (e){
 
             e.preventDefault();
@@ -176,63 +181,33 @@ async function loginUser(e) {
         }
     }
 
-          async function getFlags (){
+    //Funktion som hämtar flaggorna ifrån REST-API
+        async function getFlags (){
 
        
-            // let countryEl=document.getElementById("country")
-            // let colorsEl=document.getElementById("colors")
-     
+            try {const response = await fetch ("http://127.0.0.1:3001/api/flags")
+                if(response.ok) {
+                const data= await response.json();
+                console.log(data);
 
-            // let country=countryEl.value
-            // let colors=colorsEl.value
-        
-
-            // let flag = {  
-            // country: country,
-            // colors: colors,
-            // }
-            // const token = localStorage.getItem("user_token")
-
-            try {const response = await fetch ("http://127.0.0.1:3001/api/flags"
-            //     ,
-            //    {
-            //     method: "GET",
-            //     headers: {
-            //         "content-type": "Application/json",
-            //         // "authorization":"Bearer " + token
-            //     },
-            //     body: JSON.stringify(flag)
-            // }
-            )
-        if(response.ok) {
-            const data= await response.json();
-            console.log(data);
-
-            displayFlags(data) }} catch {
+                displayFlags(data) }} catch {
                 console.log("fel");
                 
-            }
-            
-        
-     
-    }
+            } }
 
 
-async function displayFlags (data) {
-     let flags = document.getElementById("flaggor")
-    flags.innerHTML="";
+        async function displayFlags (data) {
+            let flags = document.getElementById("flaggor")
+            flags.innerHTML="";
 
 
-    data.forEach(flag => {
-         let newElLi= document.createElement ("li")      //Skapar nytt element (li)
-           let newText=document.createTextNode (flag.country+ " " + flag.colors ) //Skapar texten till det som visas i listan
+            data.forEach(flag => {
+            let newElLi= document.createElement ("li")      //Skapar nytt element (li)
+            let newText=document.createTextNode (flag.country+ " " + flag.colors ) //Skapar texten till det som visas i listan
              //newElLi.setAttribute('id', flag.id) //Skapar attributet id
             newElLi.appendChild(newText) //Lägger newText som "barn" till newElLi
-          flags.appendChild(newElLi) // Lägger newElLi som "barn" till expEl
-    })
-        
-
-    } 
+            flags.appendChild(newElLi) // Lägger newElLi som "barn" till expEl
+    }) } 
 
 
 
